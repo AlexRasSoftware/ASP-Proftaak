f5f9eb7f9e369d448e54e61d0688ba430a6c4aa9
AwesomeKevin <k.kopp@student.fontys.nl>
2015-06-15 12:43:12 +0200

    page load eventbeheerform2

--- ICT4Events ASP Groep E S24/EventBeheerForm2.aspx.cs
+++ ICT4Events ASP Groep E S24/EventBeheerForm2.aspx.cs
@@ -9,10 +9,35 @@ namespace ICT4Events_ASP_Groep_E_S24
 {
     public partial class EventBeheerForm2 : System.Web.UI.Page
     {
+        DatabaseKoppeling database;
+        private Administratie administratie;
+        Timer timer;
+        int interval = 3000;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            administratie = new Administratie();
+            database = new DatabaseKoppeling();
+            refreshCbEvents();
+            timer = new Timer();
+            timer.Tick += timer_Tick;
         }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void refreshCbEvents()
+        {
+            ddlEvents.Items.Clear();
+            foreach (Event ev in administratie.Events)
+            {
+                ddlEvents.Items.Add(ev.Naam);
+            }
+            ddlEvents.SelectedIndex = 0;
+        }
+
+
 
         protected void imbtnCalendarStart_Click(object sender, ImageClickEventArgs e)
         {
