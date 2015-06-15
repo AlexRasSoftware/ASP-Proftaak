566bbf4d66e635bae91f9e2bce132bb11d8d589a
Siebren Weertman <siebren.w@gmail.com>
2015-06-15 13:02:53 +0200

    delete wtf code

--- ICT4Events ASP Groep E S24/EventBeheerForm2.aspx.cs
+++ ICT4Events ASP Groep E S24/EventBeheerForm2.aspx.cs
@@ -9,7 +9,6 @@ namespace ICT4Events_ASP_Groep_E_S24
 {
     public partial class EventBeheerForm2 : System.Web.UI.Page
     {
-<<<<<<< HEAD
         private Plaats selectedPlaats;
         private Event selectedEvent;
         private Administratie administartie;
@@ -52,38 +51,9 @@ namespace ICT4Events_ASP_Groep_E_S24
             {
                 ddlEvents.SelectedIndex = 0;
             }
-=======
-        DatabaseKoppeling database;
-        private Administratie administratie;
-        Timer timer;
-        int interval = 3000;
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            administratie = new Administratie();
-            database = new DatabaseKoppeling();
-            refreshCbEvents();
-            timer = new Timer();
-            timer.Tick += timer_Tick;
+
         }
-
-        void timer_Tick(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
->>>>>>> origin/master
-        }
-
-        private void refreshCbEvents()
-        {
-            ddlEvents.Items.Clear();
-            foreach (Event ev in administratie.Events)
-            {
-                ddlEvents.Items.Add(ev.Naam);
-            }
-            ddlEvents.SelectedIndex = 0;
-        }
-
-
-
+        #region calendars
         protected void imbtnCalendarStart_Click(object sender, ImageClickEventArgs e)
         {
             if (calStart.Visible) calStart.Visible = false;
@@ -107,6 +77,7 @@ namespace ICT4Events_ASP_Groep_E_S24
             tbEvDaEind.Text = calEind.SelectedDate.ToShortDateString();
             calEind.Visible = false;
         }
+        #endregion
 
         protected void btnNavLogin_Click(object sender, EventArgs e)
         {
